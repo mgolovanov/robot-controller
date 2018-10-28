132c2c1def8ab3732745b0418407c3c27303fa93
Max Golovanov <maxgolov@microsoft.com>
2018-10-27 17:17:06 -0700

    MiP robot drafts

---
+++ index.js
@@ -0,0 +1,28 @@
+var wowweemip = require("wowweemip");
+var mipFinder = new wowweemip.Finder();
+
+console.log("mipFinder:", mipFinder);
+
+console.log("Trying to scan...");
+mipFinder.scan(function(err, robots) {
+    if (err != null) {
+	console.log(err);
+	return;
+    }
+    
+    //connect to first mip
+    var selectedMip = robots[0];
+    mipFinder.connect(selectedMip, function(err) {
+	if (err != null) {
+	    console.log(err);
+	    return;
+	}
+	
+	console.log("connected");
+	
+	//move toward
+	selectedMip.driveDistanceByCm(20, 0, function(err) {
+	    console.log("moving toward");
+	});
+    });
+});
